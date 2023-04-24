0a94d8d111bc29e25a2717930b1a1c996eb91e02
armelant <56300747+armelant@users.noreply.github.com>
2023-04-24 11:13:29 +0300

    Delete file1.py

--- file1.py
+++
@@ -1,7 +0,0 @@
-import random
-import string
-
-n1 = int(input("Password lenght: "))
-l1 = string.ascii_letters + string.digits
-pas = ''.join(random.choice(l1) for i in range(n1))
-print("Your generated password:", pas)
